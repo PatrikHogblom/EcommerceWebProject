cce22a442474ce123b0304aa9b7ee88eab9e9df1
PatrikHogblom <patrik.hogblom@live.se>
2025-03-14 10:05:06 +0100

    added components, navbar and route to login and signup pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { DemoAngularMaterialModule } from './DemoAngularMaterialModule';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    DemoAngularMaterialModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
